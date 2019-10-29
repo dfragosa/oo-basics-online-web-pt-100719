75943922b0c1416c383f1608db7a6ec1a41af7d9
Derric Fragosa <fderric@gmail.com>
2019-10-29 07:26:57 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,11 @@ class Shoe
   end 
   
   def cobble
+    if @condition == old
     puts "The shoe has been repaired"
-    
+    else 
     puts "Your shoe is as good as new!"
+     end 
   end 
   
   
